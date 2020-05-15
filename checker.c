4d951ca94024a206a4bd260d97182e54851dfbeb
Terence Philander <terence.philander@momentum.co.za>
2020-05-15 12:21:58 +0200

    updates

--- checker.c
+++ checker.c
@@ -33,7 +33,7 @@ int	is_ordered(int stacka[], int top) {
     return (1);
 }
 
-void stack_init_2(int stack[2][500], int top[2], int argc, char **argv) {
+void stack_init_2(int stack[2][5000], int top[2], int argc, char **argv) {
     int i;
 
     i = 0;
@@ -50,16 +50,16 @@ void stack_init_2(int stack[2][500], int top[2], int argc, char **argv) {
     }
 }
 
-void stack_init_1(int stack[2][500], int top[2], char **argv) {
+void stack_init_1(int stack[2][5000], int top[2], char **argv) {
     int i;
     char **array;
 
     i = 0;
     array = ft_strsplit(argv[1], ' ');
     while (array[i]) {
-        if ((!ft_atoi(argv[top[2] + 1]) && argv[top[2] + 1][0] != '0') || \
-                    ft_atoi(argv[top[2] + 1]) < 0 || \
-                    ft_strlen(argv[top[2] + 1]) > 10) {
+        if ((!ft_atoi(array[i]) && array[i][0] != '0') || \
+                    ft_atoi(array[i]) < 0 || \
+                    ft_strlen(array[i]) > 10) {
             write(1, "Error", 5);
             exit(0);
         }
@@ -68,10 +68,8 @@ void stack_init_1(int stack[2][500], int top[2], char **argv) {
         top[0]++;
     }
     i--;
-    while (i >= 0) {
-        free(array[i]);
-        i--;
-    }
+    while (i >= 0)
+        free(array[i--]);
     free(array);
 }
 
@@ -99,32 +97,26 @@ void apply_command(int stacka[], int stackb[], int top[], char **line) {
 }
 
 int main(int argc, char **argv) {
-    int stack[2][500];
+    int stack[2][5000];
     int top[2];
     char *line[1000000];
 
     top[0] = -1;
     top[1] = -1;
-
     if (argc == 2)
         stack_init_1(stack, top, argv);
     else if (argc > 2)
         stack_init_2(stack, top, argc, argv);
     else
         return(0);
-
-    if (has_duplicate(stack[0], top[0])) {
+    if (has_duplicate(stack[0], top[0]))
         write(1, "Error", 5);
-        return (0);
-    }
-
+    if (!has_duplicate(stack[0], top[0]))
     while (get_next_line(0, line) >= 1)
         apply_command(stack[0], stack[1], top, line);
-    if (is_ordered(stack[0], top[0]) && top[1] == -1)
+    if (!has_duplicate(stack[0], top[0]) && is_ordered(stack[0], top[0]) && top[1] == -1)
         write(1, "OK", 2);
-    else
+    else if (!has_duplicate(stack[0], top[0]))
         write(1, "KO", 2);
-
     return 0;
 }
-
--- push_swap.c
+++ push_swap.c
@@ -1,5 +1,49 @@
 #include "libft/libft.h"
 
+int		has_duplicate(int stacka[], int top) {
+    int i;
+    int j;
+
+    i = 0;
+    j = 1;
+    while (i < top)
+    {
+        while (j <= top)
+        {
+            if (stacka[i] == stacka[j])
+                return (1);
+            j++;
+        }
+        i++;
+        j = i + 1;
+    }
+    return (0);
+}
+
+int	is_ordered(int stacka[], int top) {
+    int i;
+
+    i = 0;
+    while (i < top) {
+        if (stacka[i] > stacka[i + 1])
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
+int		in_chunk2(int stacka, int chunks[2500]) {
+    int		j;
+
+    j = 0;
+    while (j < 2500) {
+        if (stacka == chunks[j])
+            return (1);
+        j++;
+    }
+    return (0);
+}
+
 int		in_chunk(int stacka, int chunks[10][50], int i) {
     int		j;
 
@@ -48,7 +92,28 @@ int		index_biggest(int stacka[], int top, int exclude_val) {
     return (biggest_index);
 }
 
-void	complete(int stacka[], int stackb[], int top[2]) {
+void	complete2(int stacka[], int stackb[], int top[]) {
+    int		ra;
+    int		rra;
+
+    ra = index_smallest(stackb, top[1], -1);
+    rra = top[0] + 1 - ra;
+    if (ra <= rra)
+        while (ra > 0) {
+            rotate(stacka, top[0]);
+            write(1, "ra\n", 3);
+            ra--;
+        }
+    else
+        while (rra-- > 0) {
+            reverse_rotate(stacka, top[0]);
+            write(1, "rra\n", 4);
+        }
+    write(1, "pb\n", 3);
+    pushB(stacka, stackb, top);
+}
+
+void	complete(int stacka[], int stackb[], int top[]) {
     int		ra;
     int		rra;
 
@@ -61,61 +126,102 @@ void	complete(int stacka[], int stackb[], int top[2]) {
             ra--;
         }
     else
-        while (rra > 0) {
+        while (rra-- > 0) {
             reverse_rotate(stackb, top[1]);
             write(1, "rrb\n", 4);
-            rra--;
         }
     write(1, "pa\n", 3);
     pushA(stacka, stackb, top);
 }
 
-void	sort(int stack[2][500], int top[2], int chunks[10][50], int chunk) {
+void	sortnew(int stack[2][5000], int top[], int chunks[3][2500], int j) {
     int		i;
     int		caught;
     int		ra;
 
-    ra = 1000;
+    ra = 7000;
     caught = 0;
     i = 0;
-    while (stack[0][i] && caught == 0) {
+    while (caught == 0) {
+        if (in_chunk2(stack[0][i], chunks[j])) {
+            ra = i;
+            caught = 1;
+        }
+        i++;
+    }
+    if (caught == 1) {
+        while (ra-- > 0) {
+            rotate(stack[0], top[0]);
+            write(1, "ra\n", 3);
+        }
+        pushB(stack[0], stack[1], top);
+        write(1, "pb\n", 3);
+    }
+}
+
+void	sort(int stack[2][5000], int top[], int chunks[10][50], int chunk) {
+    int		i;
+    int		caught;
+    int		ra;
+
+    ra = 7000;
+    caught = 0;
+    i = 0;
+    while (caught == 0) {
         if (in_chunk(stack[0][i], chunks, chunk)) {
             ra = i;
             caught = 1;
         }
         i++;
     }
-    while (ra > 0) {
+    while (ra-- > 0) {
         rotate(stack[0], top[0]);
         write(1, "ra\n", 3);
-        ra--;
     }
     pushB(stack[0], stack[1], top);
     write(1, "pb\n", 3);
 }
 
-void	sort_2(int stack[2][500], int top[2], int chunks[10][50]) {
+void	sort_2(int stack[2][5000], int top[], int chunks[10][50]) {
     int		i;
     int		j;
 
     j = 0;
     i = 0;
     while (j <= (chunks[9][49] == 0 ? 4 : 9)) {
-        while (i <= (chunks[9][49] == 0 ? 19 : 49)) {
+        while (i++ <= (chunks[9][49] == 0 ? 19 : 49))
             sort(stack, top, chunks, j);
-            i++;
-        }
         j++;
         i = 0;
     }
     i = 0;
-    while (i <= (chunks[9][49] == 0 ? 99 : 499)) {
+    while (i++ <= (chunks[9][49] == 0 ? 99 : 499))
         complete(stack[0], stack[1], top);
-        i++;
-    }
 }
 
-void	make_chunks(int stacka[], int top[2], int chunks[10][50]) {
+void	make_chunks2(int stacka[], int top[], int chunks[3][2500]) {
+    int			i;
+    int			j;
+
+    i = 0;
+    j = 1;
+    chunks[0][0] = stacka[index_smallest(stacka, top[0], -1)];
+    while (i < 3) {
+        while (j < (top[0] + 1) / 4) {
+            chunks[i][j] = stacka[index_smallest(stacka, top[0], chunks[i][j - 1])];
+            j++;
+        }
+        i++;
+        j = 1;
+        if (i != 3)
+            chunks[i][j - 1] = stacka[index_smallest(stacka, top[0], chunks[i - 1][((top[0] + 1) / 4) - 1])];
+    }
+    i = 0;
+    j = 0;
+    //
+}
+
+void	make_chunks(int stacka[], int top[], int chunks[10][50]) {
     int			i;
     int			j;
 
@@ -134,7 +240,7 @@ void	make_chunks(int stacka[], int top[2], int chunks[10][50]) {
     }
 }
 
-void	sort5(int stacka[], int stackb[], int top[2]) {
+void	sort5(int stacka[], int stackb[], int top[]) {
     int		i;
 
     i = index_smallest(stacka, top[0], -1);
@@ -174,7 +280,7 @@ void	sort3(int stacka[]) {
         write(1, "ra\nsa\n", 6);
 }
 
-void stack_init_2(int stack[2][500], int top[2], int argc, char **argv) {
+void stack_init_2(int stack[2][5000], int top[], int argc, char **argv) {
     int i;
 
     i = 0;
@@ -185,46 +291,59 @@ void stack_init_2(int stack[2][500], int top[2], int argc, char **argv) {
             write(1, "Error", 5);
             exit(0);
         }
-        stack[0][i] = ft_atoi(argv[i + 1]);
-        i++;
-        top[0]++;
+    stack[0][i] = ft_atoi(argv[i + 1]);
+    i++;
+    top[0]++;
     }
 }
 
-void stack_init_1(int stack[2][500], int top[2], char **argv) {
+void stack_init_1(int stack[2][5000], int top[], char **argv) {
     int i;
     char **array;
 
     i = 0;
     array = ft_strsplit(argv[1], ' ');
     while (array[i]) {
+        if ((!ft_atoi(array[i]) && array[i][0] != '0') || \
+                    ft_atoi(array[i]) < 0 || \
+                    ft_strlen(array[i]) > 10) {
+            write(1, "Error", 5);
+            exit(0);
+        }
         stack[0][i] = ft_atoi(array[i]);
         i++;
         top[0]++;
     }
     i--;
-    while (i >= 0) {
-        free(array[i]);
-        i--;
-    }
+    while (i >= 0)
+        free(array[i--]);
     free(array);
 }
 
 int main(int argc, char **argv) {
-    int stack[2][500];
-    int top[2];
+    int stack[2][5000];
+    int top[3];
     int chunks[10][50];
 
     top[0] = -1;
     top[1] = -1;
-    if ((argc == 2 && ft_strlen(argv[1]) < 5) || argc == 5 || argc == 11)
-        return (0);
+    int chunks2[3][2500] = {0};
     if (argc == 2)
         stack_init_1(stack, top, argv);
-    if (argc > 2)
+    else if (argc > 2)
         stack_init_2(stack, top, argc, argv);
-
-    if (top[0] == 2)
+    else
+        return (0);
+    if (has_duplicate(stack[0], top[0])) {
+        write(1, "Error", 5);
+        return (0);
+    }
+    top[2] = 0;
+    if (is_ordered(stack[0], top[0]))
+        return (0);
+    if (top[0] == 1)
+        write(1, "sa\n", 3);
+    else if (top[0] == 2)
         sort3(stack[0]);
     else if (top[0] == 4) {
         sort5(stack[0], stack[1], top);
@@ -232,10 +351,26 @@ int main(int argc, char **argv) {
         sort3(stack[0]);
         write(1, "pa\npa\n", 6);
     }
+    else if (top[0] != 99 && top[0] != 499) {
+        make_chunks2(stack[0], top, chunks2);
+        while (top[2]++ < (top[0] + 1) / 4)
+            sortnew(stack, top, chunks2, 0);
+        top[2] = 0;
+        while (top[2]++ < (top[0] + 1) / 4)
+            sortnew(stack, top, chunks2, 1);
+        top[2] = 0;
+        while (top[2]++ < (top[0] + 1) / 4)
+            sortnew(stack, top, chunks2, 2);
+        while (top[0] != -1) {
+            write(1, "pb\n", 3);
+            pushB(stack[0], stack[1], top);
+        }
+        while (top[1] != -1)
+            complete(stack[0], stack[1], top);
+    }
     else {
         make_chunks(stack[0], top, chunks);
         sort_2(stack, top, chunks);
     }
-
     return (0);
 }
